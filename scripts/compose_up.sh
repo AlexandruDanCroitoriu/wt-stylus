75d32f4cc24488742915dbecdbfd192c47228602
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2025-05-31 17:57:36 +0300

    save

--- scripts/compose_up.sh
+++ scripts/compose_up.sh
@@ -13,5 +13,5 @@ export DOCKER_SOCK_GROUP_ID=$(stat -c '%g' /var/run/docker.sock)
 chmod -R 777 ./containers/
 
 docker compose down
-docker compose build --no-cache wt_build wt_cv -d
+docker compose build --no-cache wt_build wt_cv
 docker compose up -d
