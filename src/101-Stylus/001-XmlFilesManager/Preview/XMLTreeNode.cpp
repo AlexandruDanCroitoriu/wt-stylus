90ffed09eeaffe36e27f0b2596755da7128f8346
Alexandru Croitoriu <croitoriu.alexandru.code@gmail.com>
2025-06-09 21:30:57 +0300

    save

--- src/101-Stylus/001-XmlFilesManager/Preview/XMLTreeNode.cpp
+++ src/101-Stylus/001-XmlFilesManager/Preview/XMLTreeNode.cpp
@@ -106,7 +106,8 @@ namespace Stylus
                     var_name->setStyleClass("template-var-name");
                     for(auto& attr : temp_var_data.attributes_)
                     {
-                        auto attr_wrapper = content_wrapper_->addWidget(std::make_unique<Wt::WContainerWidget>());
+                        auto attr_wrapper = label_wrapper_->addWidget(std::make_unique<Wt::WContainerWidget>());
+                        // auto attr_wrapper = content_wrapper_->addWidget(std::make_unique<Wt::WContainerWidget>());
                         attr_wrapper->setStyleClass("flex items-center flex-nowrap text-nowrap template-var-attribute");
                         auto attr_name = attr_wrapper->addWidget(std::make_unique<Wt::WText>(attr.first + "="));
                         if(attr.first.compare("message") == 0)
